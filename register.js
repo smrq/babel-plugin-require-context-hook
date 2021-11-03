13fe02857676c6412a055631fd1e7d6f6130cca8
Tom Coleman <tom@thesnail.org>
2021-11-03 16:54:03 +1100

    Add support for webpack's `require.resolve()`

--- register.js
+++ register.js
@@ -1,44 +1,43 @@
-function context(
-	basedir,
-	directory,
-	useSubdirectories = false,
-	regExp = /^\.\//
-) {
-	const path = require('path');
-	const fs = require('fs');
+function context(basedir, directory, useSubdirectories = false, regExp = /^\.\//) {
+  const path = require('path');
+  const fs = require('fs');
 
-	function enumerateFiles(basedir, dir) {
-		let result = [];
-		fs.readdirSync(path.join(basedir, dir)).forEach(function(file) {
-			const relativePath = dir + '/' + file;
-			const stats = fs.lstatSync(path.join(basedir, relativePath));
-			if (stats.isDirectory()) {
-				if (useSubdirectories) {
-					result = result.concat(enumerateFiles(basedir, relativePath));
-				}
-			} else if (regExp.test(relativePath)) {
-				result.push(relativePath);
-			}
-		});
-		return result;
-	}
+  function enumerateFiles(basedir, dir) {
+    let result = [];
+    fs.readdirSync(path.join(basedir, dir)).forEach(function (file) {
+      const relativePath = dir + '/' + file;
+      const stats = fs.lstatSync(path.join(basedir, relativePath));
+      if (stats.isDirectory()) {
+        if (useSubdirectories) {
+          result = result.concat(enumerateFiles(basedir, relativePath));
+        }
+      } else if (regExp.test(relativePath)) {
+        result.push(relativePath);
+      }
+    });
+    return result;
+  }
 
-	const absoluteDirectory = path.resolve(basedir, directory);
-	const keys = enumerateFiles(absoluteDirectory, '.');
+  const absoluteDirectory = path.resolve(basedir, directory);
+  const keys = enumerateFiles(absoluteDirectory, '.');
 
-	function requireContext(key) {
-		if (!keys.includes(key)) {
-			throw new Error(`Cannot find module '${key}'.`);
-		}
-		const fullKey = require('path').resolve(absoluteDirectory, key);
-		return require(fullKey);
-	}
+  function requireContext(key) {
+    if (!keys.includes(key)) {
+      throw new Error(`Cannot find module '${key}'.`);
+    }
+    const fullKey = require('path').resolve(absoluteDirectory, key);
+    return require(fullKey);
+  }
 
-	requireContext.keys = () => keys;
+  requireContext.keys = () => keys;
 
-	return requireContext;
+  // path.join() strips off a leading `./`, which webpack does not do
+  const prefix = directory.startsWith('./') ? './' : '';
+  requireContext.resolve = (filename) => prefix + path.join(directory, filename);
+
+  return requireContext;
 }
 
 module.exports = function register() {
-	global.__requireContext = context;
+  global.__requireContext = context;
 };
